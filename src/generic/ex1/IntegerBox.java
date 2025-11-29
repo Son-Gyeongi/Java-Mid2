bf022c64f579f949c661dbc1bdbb9d039fe9301b
Son-Gyeongi <thsruddl77@gmail.com>
2025-11-29 09:08:21 +0900

    section2 5. 제네릭이 필요한 이유

---
+++ src/generic/ex1/IntegerBox.java
@@ -0,0 +1,15 @@
+package generic.ex1;
+
+// 숫자 보관
+public class IntegerBox {
+
+    private Integer value;
+
+    public void set(Integer value) {
+        this.value = value;
+    }
+
+    public Integer get() {
+        return value;
+    }
+}
