ee828a6d1375c6d63adfd7ebf674c140129a3ed7
Son-Gyeongi <thsruddl77@gmail.com>
2025-12-01 18:57:58 +0900

    section2 9. 제네릭 활용 예제

---
+++ src/generic/ex2/Box.java
@@ -0,0 +1,15 @@
+package generic.ex2;
+
+// 제네릭 클래스
+public class Box<T> {
+
+    private T value;
+
+    public void set(T value) {
+        this.value = value;
+    }
+
+    public T get() {
+        return value;
+    }
+}
